6f5dbcb68369237d87d3dd3197859f728b0cf792
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-06 01:25:28 +0300

    Add files via upload
    Программа считатет рубли, подаренные оператором при пополнении баланса от 100 р

---
+++ RoubleCounter.java
@@ -0,0 +1,7 @@
+public class RoubleCounter {
+
+    public int calculate (int summa, int cost ){
+        int bonus = summa/cost; // summa - сумма пополнения, cost  - стоимость 1 подаренного руб
+        return bonus;
+    }
+}
